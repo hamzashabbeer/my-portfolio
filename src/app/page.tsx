db9ae9f94e682e20a4d373388ae3c6b01aa8bd0d
hamzashabbeer <hamzashabbeer@gmail.com>
2025-01-02 23:55:23 +0500

    Add all sections to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { Navbar } from "@/components/layout/navbar";
 import { Hero } from "@/components/sections/hero";
+import { About } from "@/components/sections/about";
+import { Skills } from "@/components/sections/skills";
+import { Projects } from "@/components/sections/projects";
+import { Contact } from "@/components/sections/contact";
 
 export default function Home() {
   return (
@@ -22,6 +26,10 @@ export default function Home() {
 
       <Navbar />
       <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </main>
   );
 }
