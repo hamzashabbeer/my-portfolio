2400b1b504a424e0ec3bdc514cd7b8f6c65c0560
hamzashabbeer <hamzashabbeer@gmail.com>
2025-01-03 10:30:24 +0500

    Fix: Theme provider type error

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,14 +2,19 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: {
+  children: React.ReactNode;
+  [key: string]: any;
+}) {
   return (
     <NextThemesProvider
       attribute="class"
       defaultTheme="dark"
-      enableSystem={true}
+      enableSystem
       {...props}
     >
       {children}
